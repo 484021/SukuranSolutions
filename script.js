6e3b320bd73d5b5df8eeff833bf26aa8c0988781
484021 <bhoopalsanthosh@gmail.com>
2024-03-24 23:00:07 -0400

    feat: contact form still not working

--- script.js
+++ script.js
@@ -34,11 +34,62 @@ for (const item of dropDowns) {
   item.addEventListener("click", onClick);
 }
 
-const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+const faqItems = Array.from(document.querySelectorAll(".cs-faq-item"));
 for (const item of faqItems) {
-    const onClick = () => {
-        item.classList.toggle('active');
-    };
-    item.addEventListener('click', onClick);
+  const onClick = () => {
+    item.classList.toggle("active");
+  };
+  item.addEventListener("click", onClick);
 }
-                                
+
+//email form
+const contactForm = document.querySelector("#contact-form");
+const submitButton = document.querySelector(".submit-btn");
+const nameInput = document.querySelector("#user_name");
+const emailInput = document.querySelector("#user_email");
+const messageInput = document.querySelector("#message");
+
+//public key
+const publicKey = "KHugbW9nLiDrCSpXo";
+const serviceId = "service_29xnzve";
+const templateId = "template_2z1h1ll";
+
+emailjs.init(publicKey);
+
+//add event listener to contact form
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitButton.innerText = "Just a moment...";
+
+  const inputFields = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  emailjs.send(serviceId, templateId, inputFields).then(
+    () => {
+      submitButton.innerText = "Message sent successfully";
+
+      nameInput.value = "";
+      emailInput.value = "";
+      messageInput.value = "";
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+});
+
+let templateParams = {
+  name: "James",
+  notes: "Check this out!",
+};
+
+emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams).then(
+  (response) => {
+    console.log("SUCCESS!", response.status, response.text);
+  },
+  (error) => {
+    console.log("FAILED...", error);
+  }
+);
